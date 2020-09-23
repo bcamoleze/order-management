1ddc5ab6dc0fa83a26db7dd46717169ba726f723
Bruno Camoleze <bruno.camoleze@gmail.com>
2020-09-23 16:41:24 -0300

    Adicionar atributo role a classe User

--- src/main/java/br/com/camoleze/ordermanager/domain/User.java
+++ src/main/java/br/com/camoleze/ordermanager/domain/User.java
@@ -3,6 +3,7 @@ package br.com.camoleze.ordermanager.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.camoleze.ordermanager.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
