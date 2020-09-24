da25612fde05206b18a7cfed9cfbdf03f777fabb
Bruno Camoleze <bruno.camoleze@gmail.com>
2020-09-24 16:29:53 -0300

    Criação da Classe UserService

---
+++ src/main/java/br/com/camoleze/ordermanager/service/util/HashUtil.java
@@ -0,0 +1,14 @@
+package br.com.camoleze.ordermanager.service.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class HashUtil {
+	
+	public static String getSecureHash(String text) {
+		
+		String hash = DigestUtils.sha256Hex(text);
+		return hash;
+		
+	}
+
+}
