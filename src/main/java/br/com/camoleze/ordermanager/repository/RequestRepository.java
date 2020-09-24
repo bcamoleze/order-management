bc17dc0c60511f4bf3a362f723fa61e5103d4298
Bruno Camoleze <bruno.camoleze@gmail.com>
2020-09-24 13:57:22 -0300

    Criação dos demais repositórios

---
+++ src/main/java/br/com/camoleze/ordermanager/repository/RequestRepository.java
@@ -0,0 +1,21 @@
+package br.com.camoleze.ordermanager.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.camoleze.ordermanager.domain.Request;
+import br.com.camoleze.ordermanager.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+	
+	// busca todos os pedidos dado um determinado usuário.
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request updateRequest(Long id, RequestState state);
+	
+}
